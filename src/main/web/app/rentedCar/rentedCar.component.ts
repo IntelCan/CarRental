decc2bdc2a650e5b0ca90ffad3569abe5263cbcd
intelcan <intelcan.idea@gmail.com>
2017-05-15 07:25:56 +0200

    Added return car v2

--- src/main/web/app/rentedCar/rentedCar.component.ts
+++ src/main/web/app/rentedCar/rentedCar.component.ts
@@ -4,17 +4,17 @@
 
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import { RentedCarService} from "./rentedCar.service";
+import {Router} from "@angular/router";
+import 'rxjs/Rx';
 
 
 interface MessageJson {
     id: any;
-    mark: string;
-    model: string;
-    rate: number;
-    price: string;
-    isRented: Boolean;
+    idCustomer: number;
+    idCar: number;
+    rentalDate: Date;
+    timeRent: number;
 }
-
 
 @Component({
     selector: 'rentedCar',
@@ -27,11 +27,20 @@ export class RentedCarComponent implements OnInit,OnDestroy{
     private jsonResponse: string;
     private messages: Array<MessageJson>;
     private _subscription;
+    private idd: number;
 
-    constructor(private _userService: RentedCarService) {}
+    private car = {
+        id:'',
+        mark: '',
+        model:'',
+        rate: '',
+        price:'',
+        isRented:''
+    };
+    constructor(private _userService: RentedCarService, private router:Router) {}
 
     ngOnInit(){
-        this._subscription = this._userService.getCars()
+        this._subscription = this._userService.getRentals()
             .subscribe(
                 (data) => {
                     //this.jsonResponse = JSON.stringify(data);
@@ -42,6 +51,46 @@ export class RentedCarComponent implements OnInit,OnDestroy{
             );
 
     }
+
+    returnCar(id, idRental):void{
+        this._userService.getOneCar(id)
+            .subscribe(
+                (data) => {
+                    this.car.id = data.id;
+                    this.car.mark = data.mark;
+                    this.car.model = data.model;
+                    this.car.rate = data.rate;
+                    this.car.price = data.price;
+                    this.car.isRented = data.isRented;
+                    this.setStatusCar(id,idRental);
+                    this.router.navigate(['/car']);
+                }
+            );
+
+    }
+
+
+    setStatusCar(data, data2):void {
+
+        this._subscription = this._userService.returnCar(data2).subscribe();
+        this._userService.setStatusNotRented(this.car)
+            .subscribe(result =>{
+                this.car = {
+                    id:'',
+                    mark: '',
+                    model:'',
+                    rate: '',
+                    price:'',
+                    isRented:''
+                };
+            });
+
+    }
+
+    wyswietl(data):void{
+        console.log(data);
+    }
+
 
     ngOnDestroy(){
         this._subscription.unsubscribe();
--- src/main/web/app/rentedCar/rentedCar.service.ts
+++ src/main/web/app/rentedCar/rentedCar.service.ts
@@ -1,22 +1,52 @@
 /**
  * Created by intelcan on 14.05.17.
  */
-import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Injectable, Inject } from '@angular/core';
+import { Http, Headers,Response } from '@angular/http';
+import {Observable} from "rxjs/Observable";
+import {Rental} from "../rental/rental";
+import {Car} from "./car";
 import 'rxjs/add/operator/map';
 
 /**
- * Created by intelcan on 13.05.17.
+ * Created by intelcan on 14.05.17.
  */
 
 @Injectable()
 export class RentedCarService{
 
+    private headers = new Headers({
+        'accept': 'application/json',
+        'content-type' : 'application/json'});
+
     constructor(private _http: Http) {}
 
-    getCars() {
-        return this._http.get('/api/car/all')
+    getRentals() {
+        return this._http.get('/api/rental/all')
             .map((res:Response) => res.json());
+    }
+
+    getOneCar(id): Observable<Car>{
+        return this._http.get('/api/car/'+id, {headers : this.headers})
+            .map(res =>  res.json())
+            .catch(this.handleError);
+    }
+
+    returnCar(id: number){
+        return this._http.delete('/api/rental/'+id, {headers : this.headers})
+            .map( () => null)
+            .catch(this.handleError);
+    }
+    setStatusNotRented(data): Observable<Car>{
+        return this._http.put('api/car/return/'+data.id,JSON.stringify(data),{headers :this.headers})
+            .map(res=>res.json())
+            .catch(this.handleError);
+
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred in RentedCar service', error);
+        return Promise.reject(error.message || error);
     }
 
 }
